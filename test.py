a0757c3e511f6c646ea0f2707c3826fd7baad8a8
Seoyul <seoyul.kim@beautydatalab.com>
2021-08-10 14:21:44 +0900

    mod: chage parameter

--- test.py
+++ test.py
@@ -1,6 +1,31 @@
-def even_number(numb):
+def even_number(string):
 	lst = []
 	for i in numb:
 		if i % 2 == 0:
 			lst.append(i)
 	return lst
+	
+def even_number(string):
+	lst = []
+	for i in numb:
+		if i % 2 == 0:
+			lst.append(i)
+	return lst
+	
+
+def even_number(string):
+	lst = []
+	for i in numb:
+		if i % 2 == 0:
+			lst.append(i)
+	return lst
+	
+
+def even_number(string):
+	lst = []
+	for i in numb:
+		if i % 2 == 0:
+			lst.append(i)
+	return lst
+	
+
