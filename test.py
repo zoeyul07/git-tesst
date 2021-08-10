4372f424aa0d03d1baaee37e9ecdc2127bdd9b44
Seoyul <seoyul.kim@beautydatalab.com>
2021-08-10 14:29:13 +0900

    mod: change function

--- test.py
+++ test.py
@@ -1,6 +1,28 @@
-def even_number(numb):
+def even_number(string):
 	lst = []
 	for i in numb:
+		if i % 2 == 0:
+			lst.append(i)
+	return lst
+
+def even_number(string):
+	list = []
+	for i in numb:
+		if i % 2 == 0:
+			list.append(i)
+	return list
+def even_number(string):
+	lst = []
+	for i in string:
+		if i % 2 == 0:
+			lst.append(i)
+	return lst
+
+
+
+def even_number(string):
+	lst = []
+	for i in string:
 		if i % 2 == 0:
 			lst.append(i)
 	return lst
